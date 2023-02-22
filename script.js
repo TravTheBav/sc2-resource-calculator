c40808730018412a64c385b18217d66d5db39d27
TravTheBav <tzito93@gmail.com>
2023-02-21 21:56:54 -0600

    ids for base cards and subgroups increment based on total bases

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let totalBases = 0;
+
 let create_base_button = document.getElementById("create-base");
 create_base_button.addEventListener("click", CreateNewBase);
 let delete_base_button = document.getElementById("delete-base");
@@ -6,7 +8,9 @@ delete_base_button.addEventListener("click", DeleteLastBase);
 function CreateNewBase() {
     let base_card = document.createElement('div');
     base_card.classList.add('base-card');
-    document.querySelector('body').appendChild(base_card);
+    totalBases++;
+    base_card.id = "base-" + totalBases.toString();
+    document.querySelector('.base-wrapper').appendChild(base_card);
 
     setBaseCardIcons();
     setBaseCardWorkerCounters(base_card);
@@ -14,8 +18,11 @@ function CreateNewBase() {
 }
 
 function DeleteLastBase() {
-    let last_base = document.querySelector('body').lastChild;
-    document.querySelector('body').removeChild(last_base);
+    let last_base = document.querySelector('.base-wrapper').lastChild;
+    document.querySelector('.base-wrapper').removeChild(last_base);
+    if (totalBases > 0) {
+        totalBases--;
+    }
 }
 
 function setBaseCardIcons() {
@@ -25,49 +32,78 @@ function setBaseCardIcons() {
     
     let gas_collection_building_icon_1 = document.createElement('img');
     gas_collection_building_icon_1.classList.add('wire-frame');
-    gas_collection_building_icon_1.setAttribute('id', 1);
     gas_collection_building_icon_1.setAttribute('src', 'assets/wireframe-terran-refinery.png');
 
     let gas_collection_building_icon_2 = document.createElement('img');
     gas_collection_building_icon_2.classList.add('wire-frame');
-    gas_collection_building_icon_2.setAttribute('id', 2);
     gas_collection_building_icon_2.setAttribute('src', 'assets/wireframe-terran-refinery.png');
 
-    document.querySelector('body').lastChild.appendChild(gas_collection_building_icon_1);
-    document.querySelector('body').lastChild.appendChild(base_building_icon);
-    document.querySelector('body').lastChild.appendChild(gas_collection_building_icon_2);
+    document.querySelector('.base-wrapper').lastChild.appendChild(gas_collection_building_icon_1);
+    document.querySelector('.base-wrapper').lastChild.appendChild(base_building_icon);
+    document.querySelector('.base-wrapper').lastChild.appendChild(gas_collection_building_icon_2);
 }
 
 function setBaseCardWorkerCounters(base_card) {
-    let gas_worker_counter_1 = document.createElement('div');
-    gas_worker_counter_1.classList.add('worker-counter');
-    gas_worker_counter_1.textContent = "0/3";
-    
-    let gas_worker_counter_2 = document.createElement('div');
-    gas_worker_counter_2.classList.add('worker-counter');
-    gas_worker_counter_2.textContent = "0/3";
+    let start_idx = (totalBases * 3) - 2;
+
+    let gas_worker_counter_left = document.createElement('div');
+    gas_worker_counter_left.classList.add('worker-counter');
+    gas_worker_counter_left.id = start_idx;
+    gas_worker_counter_left.textContent = "0/3";
     
     let mineral_worker_counter = document.createElement('div');
+    mineral_worker_counter.classList.add('worker-counter');
+    mineral_worker_counter.id = start_idx + 1;
     mineral_worker_counter.textContent = "0/16";
-    mineral_worker_counter.classList.add('worker-counter');    
 
-    base_card.appendChild(gas_worker_counter_1);
+    let gas_worker_counter_right = document.createElement('div');
+    gas_worker_counter_right.classList.add('worker-counter');
+    gas_worker_counter_right.id = start_idx + 2;
+    gas_worker_counter_right.textContent = "0/3";        
+
+    base_card.appendChild(gas_worker_counter_left);
     base_card.appendChild(mineral_worker_counter);
-    base_card.appendChild(gas_worker_counter_2);
+    base_card.appendChild(gas_worker_counter_right);
 }
 
 function setBaseCardButtons(base_card) {
-    for (let i = 1; i < 4; i++) {
+    for (let i = (totalBases * 3) - 2; i < (totalBases * 3) + 1; i++) {
         let button_group = document.createElement('div');
         button_group.classList.add('up-down-buttons');
-        button_group.id = i;
+        button_group.id = "button-group-" + i.toString();
         
         let down_button = document.createElement('button');
+        down_button.classList.add('down');
         down_button.textContent = "-";
+        down_button.addEventListener('click', function() { workerButtonDownClick(base_card.id, button_group.id); });
+        
         let up_button = document.createElement('button');
+        up_button.classList.add('up');
         up_button.textContent = "+";
+        up_button.addEventListener('click', function() { workerButtonUpClick(base_card.id, button_group.id); });
+
         button_group.appendChild(down_button);
         button_group.appendChild(up_button);
         base_card.appendChild(button_group);
     }
+}
+
+function workerButtonDownClick(base_card_id, button_group_id) {
+    let worker_counter = document.querySelector(`div#${base_card_id}.base-card div#${button_group_id}.worker-counter`);
+    let str = worker_counter.textContent;
+    let numerator = parseInt(str.split("/")[0]);
+    let denominator = str.split("/")[1];
+    if (numerator > 0) {
+        numerator--;
+    }
+    worker_counter.textContent = numerator.toString() + "/" + denominator;
+}
+
+function workerButtonUpClick(base_card_id, button_group_id) {
+    let worker_counter = document.querySelector(`div#${base_card_id}.base-card div#${button_group_id}.worker-counter`);
+    let str = worker_counter.textContent;
+    let numerator = parseInt(str.split("/")[0]);
+    let denominator = str.split("/")[1];
+    numerator++;
+    worker_counter.textContent = numerator.toString() + "/" + denominator;
 }
